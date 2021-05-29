5c2c08f85fa6b08cacbadb67d9bae9509e7062f0
celestialheathen <jingchaoyu@hotmail.com>
2021-05-29 15:07:46 -0400

    sign up page

--- bot.rb
+++ bot.rb
@@ -1,14 +1,13 @@
 require "selenium-webdriver"
 require "chromedriver-helper"
-require "dotenv"
-Dotenv.load
+require "dotenv/load"
 
 driver = Selenium::WebDriver.for :chrome 
-driver.get "https://www.dndbeyond.com/characters/builder#/"
+driver.get "https://www.dndbeyond.com/"
 
 driver.find_element(id: "login-link").click
 
 driver.find_element(id: "signin-with-google").click
 
 driver.switch_to.window(driver.window_handles[1])
-driver.find_element(id: "identifierId").send_keys "jingchaoyu@hotmail.com"
+driver.find_element(id: "identifierId").send_keys "#{ENV['EMAIL']}"
