03c81e39d15a46ad4f6b6faee751a2b5ce3efd9c
celestialheathen <jingchaoyu@hotmail.com>
2021-05-29 17:34:34 -0400

    commit

--- bot.rb
+++ bot.rb
@@ -11,3 +11,11 @@ driver.find_element(id: "signin-with-google").click
 
 driver.switch_to.window(driver.window_handles[1])
 driver.find_element(id: "identifierId").send_keys "#{ENV['EMAIL']}"
+
+driver.find_element(id: "identifierNext").click
+
+wait = Selenium::WebDriver::Wait.new(timeout: 10)
+wait.until { driver.find_element(name: "password").displayed? }
+
+driver.find_element(name: "password").send_keys "#{ENV['PASSWORD']}"
+driver.find_element(id: "passwordNext").click
